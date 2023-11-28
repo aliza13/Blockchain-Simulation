8bca65db367183dfe2324f4910ce641b61660c3d
aliza13 <alizaajc13@gmail.com>
2023-11-28 15:53:03 -0800

    Update timeTesting.c

--- testing/timeTesting.c
+++ testing/timeTesting.c
@@ -3,7 +3,6 @@
   
 int main() 
 { 
-  
     struct tm* local; 
     time_t t = time(NULL); 
   
@@ -15,3 +14,18 @@ int main()
   
     return 0; 
 } 
+
+time_t get_current_time() {
+        struct tm* local; 
+    time_t t = time(NULL); 
+  
+    // Get the localtime 
+    local = localtime(&t); 
+  
+    printf("Local time and date: %s\n", 
+           asctime(local)); 
+  
+    printf("%i", local);
+
+    return local;
+}
