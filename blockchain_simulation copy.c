1e6702bb9e42e9178c9348df748b6eb5ead2121f
j-s-mart <jakestephenmartin@gmail.com>
2023-11-28 20:06:52 -0800

    deleted old copy file, made a new one

--- blockchain_simulation copy.c
+++ blockchain_simulation copy.c
@@ -5,51 +5,104 @@
 #include <time.h>
 #include <windows.h>
 #include <bcrypt.h>
+#include <stdbool.h>
 
 #define NT_SUCCESS(Status) (((NTSTATUS)(Status)) >= 0) // returns true if >= 0
 #define STATUS_UNSUCCESSFUL ((NTSTATUS)0xC0000001L) // error code
 
 typedef struct hash_info_needed {
-    time_t timestamp;
-    float data;
+    char timestamp[50];
+    double data;
     char *previous_hash;
 } hash_info_needed;
 
 typedef struct block_node {
-    time_t timestamp;
-    float data; // Cee is currency, data is amount
-    char *previous_hash;
+    char timestamp[50];
+    double data; // Cee is currency, data is amount
+    char *hash[255];
+    char *previous_hash[255];
     struct block_node *next;
 } block_node;
 
-// void make_hash(block_node *block);
 char* make_hash(hash_info_needed *block);
-
-void print_block(const block_node *block) {
-    printf("Timestamp: %ld\n", block->timestamp);
-    printf("Amount: %.2f\n", block->data);
-    printf("Previous Hash: %s\n", block->previous_hash);
-}
+block_node* add_block(block_node *head, char new_timestamp[50], double new_data, char *new_hash[255], char *new_previous_hash[255]);
+void show_menu(void);
+int clear_buffer(void);
+int get_int(const char *prompt);
+double get_float(const char *prompt);
+void print_block(const block_node *block);
+void quit(void);
 
 int main() {
+    printf("Welcome to the Blockchain Simulator!\n\n");
+    show_menu();
 
-    // set genesis block to NULL values besides .next to point to 1st node
+    char previous_hash[255] = "";
+    double data;
+    char timestamp;
+    time_t t;
+    char current_time[50];
+    char temp_hash[255] = "";
 
-    // sample block for now
-    hash_info_needed myBlock;
-    myBlock.timestamp = 12345678910;
-    myBlock.data = 5.2;
-    myBlock.previous_hash = NULL;
+    block_node *head_ptr = NULL;
+
+    // Make the genesis block:
+    time(&t);
+    strcpy(current_time, ctime(&t));
+    hash_info_needed newHash;
+    strcpy(newHash.timestamp, current_time);
     
-    // create the hash and get it as a hexadecimal stringS
-    char* hexHash = make_hash(&myBlock);
+    newHash.data = data;
+    newHash.previous_hash = previous_hash;
 
-    // print hash to term
-    if (hexHash) {
-        printf("Hash: %s\n", hexHash);
-        HeapFree(GetProcessHeap(), 0, hexHash); // Free the memory allocated for the hexadecimal string
-    }
-    
+    strcpy(temp_hash, make_hash(&newHash));
+    head_ptr = add_block(head_ptr, current_time, 0, temp_hash, previous_hash);
+    strcpy(previous_hash, temp_hash); 
+
+    while (true)
+    {
+        int choice = get_int("\nWhat would you like to do? (Choose an option): ");
+        enum {ADD = 1, DISPLAY, MENU, QUIT};
+        
+        switch (choice)
+        { 
+            case ADD:
+                time(&t);
+                strcpy(current_time, ctime(&t));
+
+                // printf("%s", current_time);
+                data = get_float("Enter amount of Cee: ");
+                // printf("Enter data: ");
+                // scanf(" %f", &data);
+                // clear_buffer();
+                // printf("Data: %.2f", data);
+
+                hash_info_needed newHash;
+                strcpy(newHash.timestamp, current_time);
+                
+                newHash.data = data;
+                newHash.previous_hash = previous_hash;
+
+                strcpy(temp_hash, make_hash(&newHash));
+
+                //printf("Previous Hash: %s", previous_hash);
+
+                head_ptr = add_block(head_ptr, current_time, data, temp_hash, previous_hash);
+                
+                strcpy(previous_hash, temp_hash);              
+                break;     
+            case DISPLAY:
+                print_block(head_ptr);
+                break;   
+            case MENU:
+                show_menu();
+                break;
+            case QUIT:
+                printf("Thanks, see you next time!");
+                quit();
+                break;
+        }
+    }    
     return 0;
 }
 
@@ -153,38 +206,141 @@ Cleanup:
     return hexHash;
 }
 
-// add_block(block_node *head, time_t new_timestamp, float new_data, char *new_previous_hash, char new_hash[255])
-// {
-//     // Create new block
-//     block_node *new_block = malloc(sizeof(block_node));
-//     if (new_block)
-//     {
-//         /*Assign the block it's values*/
-//         new_block->timestamp = new_timestamp;
-//         new_block->data = new_data;
-//         // PREVIOUS HASH FIELD NEEDED
-//         // HASH FIELD NEEDED
-//         new_block->next = NULL;
-//         // If linked list is empty, the new block is the genesis block.
-//         if (head == NULL)
-//         {
-//             //new_block->previous_hash = NULL;
-//             return new_block;
-//         }
-//         // Otherwise, iterate through the chain and add the block at the end.
-//         else
-//         {
-//             block_node *current = head;
-//             while (current->next != NULL)
-//             {
-//                 current = current->next;
-//             }
-//             current->next = new_block;
-//             return head;
-//         }
-//     }
-//     else
-//     {
-//         return NULL;
-//     }
-// }
+block_node* add_block(block_node *head, char new_timestamp[50], double new_data, char *new_hash[255], char *new_previous_hash[255])
+{
+    // Create new block
+    block_node *new_block = malloc(sizeof(block_node));
+    if (new_block)
+    {
+        /*Assign the block it's values*/
+        
+        //printf("\nNew Timestamp: %s", new_timestamp);
+        strcpy(new_block->timestamp, new_timestamp);
+        //printf("\nBlock Time: %s", new_block->timestamp);
+
+        //printf("\nNew Data: %f", new_data);
+        new_block->data = new_data;
+        //printf("\nBlock Data: %f", new_block->data);
+
+        strcpy(new_block->hash, new_hash);
+
+        //printf("\nNew Prev Hash: %s", new_previous_hash);
+        strcpy(new_block->previous_hash, new_previous_hash);
+        //printf("\nBlock Prev Hash: %s", new_block->previous_hash);
+
+        new_block->next = NULL;
+        // If linked list is empty, the new block is the genesis block.
+        if (head == NULL)
+        {
+            //new_block->previous_hash = NULL;
+            return new_block;
+        }
+        //Otherwise, iterate through the chain and add the block at the end.
+        else
+        {
+            block_node *current = head;
+            while (current->next != NULL)
+            {
+                current = current->next;
+            }
+            current->next = new_block;
+            return head;
+        }
+    }
+    else
+    {
+        return NULL;
+    }
+}
+
+void show_menu(void)
+{
+    printf("************ Blockchain  Simulation ************\n");
+    printf("*               1: Add Block                   *\n");
+    printf("*               2: Display Block Info          *\n");
+    printf("*               3: Show Menu                   *\n");
+    printf("*               4: Quit                        *\n");
+    printf("************************************************\n");
+}
+
+int clear_buffer(void)
+{
+    int ch;
+    int count = 0;
+    while((ch = getchar()) != '\n' && ch != EOF)
+        ++count;
+    
+    return count;
+}
+
+int get_int(const char *prompt)
+{
+    int num;
+    int ret;
+    int count;
+
+    do
+    {
+        printf("%s", prompt);
+        ret = scanf("%d", &num);
+
+        /* Clear buffer */
+        count = clear_buffer();
+
+        /**
+         * If scanf() did not successfully read one integer
+         * or, if clear_buffer() reads at least one non-white character.
+         */
+        if (ret != 1 || count > 0)
+            printf("Invalid input! Please enter just one integer from 1 to 6.\n");
+
+    } while(ret != 1 || count != 0);
+
+    return num;
+}
+
+double get_float(const char *prompt)
+{
+    double num;
+    int ret;
+    int count;
+
+    do
+    {
+        printf("%s", prompt);
+        ret = scanf("%lf", &num);
+
+        /* Clear buffer */
+        count = clear_buffer();
+
+        /**
+         * If scanf() did not successfully read one integer
+         * or, if clear_buffer() reads at least one non-white character.
+         */
+        if (ret != 1 || count > 0)
+            printf("Invalid input! Please enter just one integer from 1 to 6.\n");
+
+    } while(ret != 1 || count != 0);
+
+    return num;
+}
+
+void print_block(const block_node *block) {
+    
+    block_node *current = block;
+
+    if (!current)
+        puts("There are no blocks in the chain.");
+    
+    while (current != NULL)
+    {
+        printf("\nTimestamp:\t%s\nAmount:\t%f\nHash:\t%s\nPrevious Hash:\t%s\n", current->timestamp, current->data, current->hash, current->previous_hash);
+        current = current->next;
+    }
+    return;
+}
+
+void quit(void)
+{
+    exit(0);
+}
